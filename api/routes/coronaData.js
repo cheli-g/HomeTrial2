7c5f9237545c770bff0eece4f7f45fa26eaf24a2
cheli-g <96668250+cheli-g@users.noreply.github.com>
2023-05-11 20:49:06 +0300

    rachel1

---
+++ api/routes/coronaData.js
@@ -0,0 +1,63 @@
+const express=require('express');
+const router=express.Router();
+const corona_model=require('../models/coronaData');
+const mongoose= require('mongoose');
+
+router.get('/',(req,res)=>{
+    corona_model.find().then((coronaData)=>{
+        res.status(200).json({
+            coronaData
+    });
+    }).catch(error=>{
+         res.status(500).json({
+             error
+         });
+    
+    })
+});
+
+router.get('/:coronaId',(req,res)=>{
+    const coronaId=req.params.coronaId;
+
+    corona_model.findById(userId).then((coronaData)=>{
+        res.status(200).json({
+            coronaData
+        })
+    }).catch(error=>{
+            res.status(500).json({
+                error
+            });
+       })
+    
+})   
+
+router.post('/',(req,res)=>{
+
+  
+    const {idNumber,timeOfVaccination,manufacturer,coronaVirusDesease} =req.body;
+    
+    const user= new corona_model({
+        _id: new mongoose.Types.ObjectId(),
+        idNumber,
+        timeOfVaccination,
+        manufacturer,
+        coronaVirusDesease
+    });
+    
+    user.save().then(()=>{
+         res.status(200).json({
+             message:'coronaData created'
+         })
+        }).catch(error=>{
+             res.status(500).json({
+                 error
+             });
+         
+     });
+
+
+
+});
+
+
+module.exports=router;
